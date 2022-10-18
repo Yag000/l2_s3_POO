d3fe63c4db186d1aafcdd79775df25849d61846d
Yago-Iglesias <101363786+Yago-Iglesias@users.noreply.github.com>
2022-10-18 23:41:24 +0200

    Td4 (#2)
    * Exercice 1 et 2
    
    * Exercice 4
    
    * Exercice 4
    
    * Exercice 1
    
    * Update ex 1 with packages
    
    * Exercice 2
    
    * Exercice 3
    
    * Exercice 6 TP4
    
    * Exercice 4 TD4
    
    * Exercice 4 TD4
    
    * Exercice 6 et commentaires

---
+++ TPs/TP4/src/LongMetrage.java
@@ -0,0 +1,19 @@
+public class LongMetrage extends Video {
+    private final String realisateur;
+
+    public LongMetrage(String titre, int duree, String realisateur) {
+        super(titre, duree);
+        this.realisateur = realisateur;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", LongMetrage realisateur= " + realisateur;
+    }
+
+    @Override
+    public int ordreMedia() {
+        return 3;
+    }
+
+}
