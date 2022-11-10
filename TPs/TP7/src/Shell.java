5fe372d7f11d5abeb162171b5075d9807b064180
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-11-10 17:52:13 +0100

    find

--- TPs/TP7/src/Shell.java
+++ TPs/TP7/src/Shell.java
@@ -1,6 +1,3 @@
-import javax.lang.model.util.ElementScanner14;
-import javax.swing.text.DefaultStyledDocument.ElementSpec;
-
 public class Shell {
 
     public Dossier root;
@@ -15,6 +12,24 @@ public class Shell {
             tmp = tmp.getParent().getParent();
 
         root = tmp;
+    }
+
+    private Entree find(String name) {
+        String[] path = name.split("/");
+        Dossier current = this.current;
+        Entree res = null;
+
+        for (String s : path) {
+            res = current.getEntree(s, false);
+            if (res == null)
+                return null;
+            if (res.getElement() instanceof Dossier)
+                current = (Dossier) res.getElement();
+
+        }
+
+        return res;
+
     }
 
     public void cat(String name) {
@@ -68,6 +83,38 @@ public class Shell {
 
     public void mkdir(String name) {
         current.ajouter(new Dossier(current.getParent()), name);
+    }
+
+    public void mv(String name, String newFolder) {
+        Entree e = current.getEntree(name, false);
+
+        if (e == null) {
+            System.out.println("File not found");
+            return;
+        }
+
+        String[] s = newFolder.split("/");
+
+        Dossier tmp = current;
+
+        for (String folder : s) {
+            Entree entree = tmp.getEntree(folder, false);
+
+            if (entree == null) {
+                System.out.println("Folder not found");
+                return;
+            }
+
+            if (entree.getElement() instanceof Dossier dossier)
+                tmp = dossier;
+            else {
+                System.out.println(entree.getNom() + " is not a folder");
+                return;
+            }
+        }
+
+        tmp.ajouter(e.getElement(), e.getNom());
+        current.supprimer(e);
     }
 
     public static void main(String[] args) {
