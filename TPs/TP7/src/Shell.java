4f0d3a7ba289abdd05e3081aa922280f2fd17a4d
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-11-11 15:42:14 +0100

    Rename variables

--- TPs/TP7/src/Shell.java
+++ TPs/TP7/src/Shell.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class Shell {
 
+    // Constantes pour les messages d'erreur
+
     private static final String IS_A_SPECIAL_FILE = " is a special file";
     private static final String NO_SUCH_FILE_OR_DIRECTORY = ": No such file or directory";
     private static final String NOT_A_DIRECTORY = ": Not a directory";
     private static final String FILE_NOT_FOUND = "File not found";
 
+    // Attributs des dossiers
+
     private final Dossier root;
     private Dossier current;
 
+    // Scanner pour gérer les entrées du Shell
+
     private Scanner scanner;
+
+    // Constructeur
+
+    public Shell() {
+        root = new Dossier();
+        current = root;
+        scanner = new Scanner(System.in);
+    }
 
     public Shell(Dossier first) {
         current = first;
@@ -36,7 +50,7 @@ public class Shell {
     // Méthodes de type Find
 
     /**
-     * Cherche un element dans l'arborescence, en gérant les / et les ...
+     * Cherche un element dans l'arborescence, en gérant les / et les ..
      * 
      * @param path Nom de l'élément à chercher
      */
@@ -58,15 +72,15 @@ public class Shell {
     }
 
     /**
-     * Cherche un element dans l'arborescence, en gérant les / et les ...
+     * Cherche un element dans l'arborescence, en gérant les / et les ..
      * 
-     * @param path Nom de l'élément à chercher
+     * @param pathList Nom de l'élément à chercher
      */
-    private Entree find(String[] path) {
+    private Entree find(String[] pathList) {
         Dossier tmp = this.current;
         Entree res = null;
 
-        for (String s : path) {
+        for (String s : pathList) {
             res = tmp.getEntree(s, false);
             if (res == null)
                 return null;
@@ -79,7 +93,7 @@ public class Shell {
     }
 
     /**
-     * Cherche un element dans l'arborescence, en gérant les / et les ...
+     * Cherche un element dans l'arborescence, en gérant les / et les ..
      * 
      * @param path  Chemin du dossier à chercher
      * @param depth Profondeur de la recherche
@@ -111,7 +125,7 @@ public class Shell {
     }
 
     /**
-     * Cherche un fichier de texte dans l'arborescence, en gérant les / et les ...
+     * Cherche un fichier de texte dans l'arborescence, en gérant les / et les ..
      * 
      * @param path
      * @return
@@ -126,7 +140,7 @@ public class Shell {
     }
 
     /**
-     * Cherche un dossier dans l'arborescence, en gérant les / et les ...
+     * Cherche un dossier dans l'arborescence, en gérant les / et les ..
      * 
      * @param path
      * @return
@@ -187,9 +201,8 @@ public class Shell {
 
     // Commandes
 
-    // Works
-    private void cat(String name) {
-        FichierTexte texte = findFichierTexte(name);
+    private void cat(String path) {
+        FichierTexte texte = findFichierTexte(path);
 
         if (texte == null)
             System.out.println(FILE_NOT_FOUND);
@@ -198,11 +211,10 @@ public class Shell {
 
     }
 
-    // Works
-    private void cd(String folderName) {
-        String[] s = folderName.split("/");
+    private void cd(String path) {
+        String[] pathList = path.split("/");
 
-        for (String folder : s) {
+        for (String folder : pathList) {
             Entree e = current.getEntree(folder, false);
 
             if (e == null) {
@@ -219,41 +231,38 @@ public class Shell {
         }
     }
 
-    // Works
     private void cd() {
         current = root;
     }
 
-    // Works
-    private void ls(String name) {
+    private void ls(String path) {
 
-        if (name == null || name.equals("")) {
+        if (path == null || path.equals("")) {
             current.afficher();
             return;
         }
 
-        Entree e = find(name);
+        Entree lastFolder = find(path);
 
-        if (e == null)
+        if (lastFolder == null)
             System.out.println("Folder not found");
-        else if (e.getElement() instanceof Dossier dossier)
+        else if (lastFolder.getElement() instanceof Dossier dossier)
             dossier.afficher();
         else
-            System.out.println(e.getNom() + " is not a folder");
+            System.out.println(lastFolder.getNom() + " is not a folder");
 
     }
 
-    // Works
-    private void mkdir(String name) {
+    private void mkdir(String path) {
 
-        if (name == null || name.equals("")) {
+        if (path == null || path.equals("")) {
             System.out.println("mkdir: missing operand");
             return;
         }
 
-        if (isAllFolders(name)) {
+        if (isAllFolders(path)) {
             Dossier tmp = current;
-            String[] pathList = name.split("/");
+            String[] pathList = path.split("/");
 
             for (String s : pathList) {
                 Entree e = tmp.getEntree(s, false);
@@ -268,127 +277,124 @@ public class Shell {
         }
     }
 
-    // Works
-    private void mv(String name, String newFolder) {
+    private void mv(String oldPath, String newPath) {
 
-        if (name == null || name.equals("")) {
+        if (oldPath == null || oldPath.equals("")) {
             System.out.println("mv: missing operand");
             return;
         }
 
-        Entree entreeOfOriginal = find(name);
+        Entree originalEntree = find(oldPath);
 
-        if (entreeOfOriginal == null) {
-            System.out.println("mv: " + name + NO_SUCH_FILE_OR_DIRECTORY);
+        if (originalEntree == null) {
+            System.out.println("mv: " + oldPath + NO_SUCH_FILE_OR_DIRECTORY);
             return;
         }
 
-        if (entreeOfOriginal instanceof EntreeSpeciale) {
-            System.out.println("mv: " + name + IS_A_SPECIAL_FILE);
+        if (originalEntree instanceof EntreeSpeciale) {
+            System.out.println("mv: " + oldPath + IS_A_SPECIAL_FILE);
             return;
         }
 
-        String[] pathList = newFolder.split("/");
+        String[] newPathList = newPath.split("/");
         Dossier newDossier;
 
-        if (pathList.length == 1) {
-            Entree tmp = current.getEntree(newFolder, false);
+        if (newPathList.length == 1) {
+            Entree tmp = current.getEntree(newPath, false);
             newDossier = tmp == null || !(tmp.getElement() instanceof Dossier) ? current : (Dossier) tmp.getElement();
         }
 
         else
-            newDossier = findLastDossier(newFolder);
+            newDossier = findLastDossier(newPath);
 
         if (newDossier == null) {
-            System.out.println("mv: " + newFolder + NO_SUCH_FILE_OR_DIRECTORY);
+            System.out.println("mv: " + newPath + NO_SUCH_FILE_OR_DIRECTORY);
             return;
         }
 
-        String newName = newDossier.getParent().getNom().equals(pathList[pathList.length - 1])
-                ? entreeOfOriginal.getNom()
-                : pathList[pathList.length - 1];
+        String newName = newDossier.getParent().getNom().equals(newPathList[newPathList.length - 1])
+                ? originalEntree.getNom()
+                : newPathList[newPathList.length - 1];
 
         // Only problem ""..""
-        newDossier.ajouter(entreeOfOriginal.getElement(), newName);
+        newDossier.ajouter(originalEntree.getElement(), newName);
 
-        entreeOfOriginal.supprimer();
+        originalEntree.supprimer();
 
         if (newDossier.getEntree(newName, false).getElement() instanceof Dossier dossier) {
             dossier.updateParentDirectory();
         }
     }
 
-    // Works
-    private void rm(String name) {
-        Entree e = find(name);
+    private void rm(String path) {
+        Entree lastEntree = find(path);
 
-        if (e instanceof EntreeSpeciale) {
-            System.out.println("mv: " + name + IS_A_SPECIAL_FILE);
+        if (lastEntree instanceof EntreeSpeciale) {
+            System.out.println("mv: " + path + IS_A_SPECIAL_FILE);
             return;
         }
 
-        if (e == null)
+        if (lastEntree == null)
             System.out.println(FILE_NOT_FOUND);
-        else if (e.getElement() instanceof FichierTexte)
-            e.supprimer();
+        else if (lastEntree.getElement() instanceof FichierTexte)
+            lastEntree.supprimer();
         else
-            System.out.println("rm: " + name + ": Is a directory");
+            System.out.println("rm: " + path + ": Is a directory");
     }
 
-    // Works
-    private void ed(String filename) {
+    private void ed(String path) {
 
-        if (filename == null || filename.equals("")) {
+        if (path == null || path.equals("")) {
             System.out.println("ed: missing operand");
             return;
         }
 
-        String[] path = filename.split("/");
+        String[] pathList = path.split("/");
 
         Entree e;
 
-        if (path.length == 1) {
-            current.ajouter(new FichierTexte(""), filename);
-            e = current.getEntree(filename, true);
+        if (pathList.length == 1) {
+            current.ajouter(new FichierTexte(""), path);
+            e = current.getEntree(path, true);
         } else {
-            Dossier dossier = findLastDossier(filename);
+            Dossier dossier = findLastDossier(path);
 
             if (dossier == null) {
-                System.out.println("ed: " + filename + NOT_A_DIRECTORY);
+                System.out.println("ed: " + path + NOT_A_DIRECTORY);
                 return;
             }
 
-            dossier.ajouter(new FichierTexte(""), path[path.length - 1]);
-            e = dossier.getEntree(path[path.length - 1], false);
+            dossier.ajouter(new FichierTexte(""), pathList[pathList.length - 1]);
+            e = dossier.getEntree(pathList[pathList.length - 1], false);
         }
 
-        FichierTexte f = (FichierTexte) e.getElement();
+        FichierTexte file = (FichierTexte) e.getElement();
 
-        if (f == null) {
+        if (file == null) {
             System.out.println(FILE_NOT_FOUND);
             return;
         }
 
         System.out.println("Entrez le texte du fichier (terminez par une ligne contenant seulement un point)");
-        f.editer(scanner, false);
+        file.editer(scanner, false);
     }
 
     // Works
-    private void cp(String oldFolder, String newFolder) {
+    private void cp(String oldPath, String newPath) {
 
-        if (oldFolder == null || oldFolder.equals("") || newFolder.equals("")) {
+        if (oldPath == null || oldPath.equals("") || newPath.equals("")) {
             System.out.println("mv: missing operand");
             return;
         }
 
-        Entree oldDossier = find(oldFolder);
-        if (oldDossier == null) {
-            System.out.println("cp: " + oldFolder + NO_SUCH_FILE_OR_DIRECTORY);
+        Entree oldEntree = find(oldPath);
+        if (oldEntree == null) {
+            System.out.println("cp: " + oldPath + NO_SUCH_FILE_OR_DIRECTORY);
             return;
         }
 
-        if (oldDossier instanceof EntreeSpeciale) {
-            System.out.println("mv: " + oldFolder + IS_A_SPECIAL_FILE);
+        if (oldEntree instanceof EntreeSpeciale) {
+            System.out.println("mv: " + oldPath + IS_A_SPECIAL_FILE);
             return;
         }
 
@@ -396,23 +402,23 @@ public class Shell {
         Dossier newDossier;
         String newName;
 
-        if (newFolder.split("/").length == 1) {
+        if (newPath.split("/").length == 1) {
             newEntree = current.getParent();
             newDossier = current;
-            newName = newFolder;
+            newName = newPath;
         } else {
-            newDossier = findLastDossier(newFolder);
+            newDossier = findLastDossier(newPath);
 
             if (newDossier == null) {
-                System.out.println("cp: " + oldFolder + NOT_A_DIRECTORY);
+                System.out.println("cp: " + oldPath + NOT_A_DIRECTORY);
                 return;
             } else {
-                newName = newFolder.split("/")[newFolder.split("/").length - 1];
-                newEntree = find(newFolder, -1);
+                newName = newPath.split("/")[newPath.split("/").length - 1];
+                newEntree = find(newPath, -1);
             }
         }
 
-        newDossier.ajouter(oldDossier.getElement().copy(newEntree), newName);
+        newDossier.ajouter(oldEntree.getElement().copy(newEntree), newName);
     }
 
     // Works
@@ -424,35 +430,35 @@ public class Shell {
     private void parser() {
         while (true) {
             System.out.print(current.getChemin() + "$ ");
-            String[] s = scanner.nextLine().split(" ");
+            String[] commande = scanner.nextLine().split(" ");
 
-            switch (s[0]) {
+            switch (commande[0]) {
                 case "cat":
-                    cat(s[1]);
+                    cat(commande[1]);
                     break;
                 case "cd":
-                    if (s.length == 1)
+                    if (commande.length == 1)
                         cd();
                     else
-                        cd(s[1]);
+                        cd(commande[1]);
                     break;
                 case "ls":
-                    ls(s.length > 1 ? s[1] : null);
+                    ls(commande.length > 1 ? commande[1] : null);
                     break;
                 case "mkdir":
-                    mkdir(s[1]);
+                    mkdir(commande[1]);
                     break;
                 case "mv":
-                    mv(s[1], s[2]);
+                    mv(commande[1], commande[2]);
                     break;
                 case "rm":
-                    rm(s[1]);
+                    rm(commande[1]);
                     break;
                 case "ed":
-                    ed(s[1]);
+                    ed(commande[1]);
                     break;
                 case "cp":
-                    cp(s[1], s[2]);
+                    cp(commande[1], commande[2]);
                     break;
                 case "pwd":
                     pwd();
@@ -470,6 +476,6 @@ public class Shell {
     }
 
     public static void main(String[] args) {
-        new Shell(new Dossier());
+        new Shell();
     }
 }
