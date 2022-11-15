fc0402aed3bc47c48eba65e3b193da08ef1b8368
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-11-15 11:39:34 +0100

    Exo 4.1

---
+++ TDs/TD8/src/Exo_4/Exo4.java
@@ -0,0 +1,32 @@
+package Exo_4;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.time.LocalTime;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+public class Exo4 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Relief Valve");
+        frame.setResizable(false);
+        frame.setLocation(500, 300);
+        JLabel label = new JLabel("this is my text");
+        frame.add(label);
+        frame.pack();
+
+        int delay = 1000; // milliseconds
+        ActionListener afficheHeure = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                System.out.println(LocalTime.now());
+            }
+        };
+
+        Timer t1 = new Timer(delay, afficheHeure);
+
+        t1.start();
+    }
+}
