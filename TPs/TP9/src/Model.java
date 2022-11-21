835622c82305eaa7fb33d35bc818a17eab7154f7
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-11-21 17:21:17 +0100

    Exo b8

---
+++ TPs/TP9/src/Model.java
@@ -0,0 +1,15 @@
+public class Model {
+    private int rouge;
+    private int vert;
+    private int bleu;
+
+    public Model() {
+    }
+
+    public Model(int rouge, int vert, int bleu) {
+        this.rouge = rouge;
+        this.vert = vert;
+        this.bleu = bleu;
+    }
+
+}
--- TPs/TP9/src/Vue.java
+++ TPs/TP9/src/Vue.java
@@ -89,6 +89,5 @@ public class Vue extends JFrame {
         initSlider(bleuSlider, "bleu");
 
         initButtons();
-
-    };
+    }
 }
