afaeb0c0ee54f8fafabd7a3a3ec3d7eae60f5519
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-11-21 16:26:09 +0100

    Exo 3.1

--- TPs/TP9/src/Vue.java
+++ TPs/TP9/src/Vue.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Vue extends JFrame {
+
+    JPanel panneauColor = new JPanel();
+
     Vue() {
         setTitle("Palette");
         setSize(800, 600);
