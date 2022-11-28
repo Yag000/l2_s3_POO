b30f0e3f50f5790354056af9e1555223b358c6ba
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-11-28 17:09:13 +0100

    Exo 5

--- TPs/TP10/src/ImageEditView.java
+++ TPs/TP10/src/ImageEditView.java
@@ -1,11 +1,15 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import java.awt.Graphics2D;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-import java.awt.image.BufferedImage;
 
 public class ImageEditView extends JFrame {
 
@@ -31,7 +35,41 @@ public class ImageEditView extends JFrame {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.drawImage(model.getImage(), 0, 0, this);
+            ((Graphics2D) g).draw(selection.getRectangle());
         }
 
+        private class Selection extends MouseAdapter implements MouseMotionListener {
+
+            int x0, x1, y0, y1;
+
+            public Rectangle getRectangle() {
+                return new Rectangle(x0, y0, x1 - x0, y1 - y0);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent event) {
+                x0 = event.getX();
+                y0 = event.getY();
+
+                cutButton.setEnabled(false);
+
+                imagePane.repaint();
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent event) {
+                x1 = event.getX();
+                y1 = event.getY();
+
+                cutButton.setEnabled(true);
+
+                imagePane.repaint();
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent event) {
+            }
+
+        }
     }
 }
