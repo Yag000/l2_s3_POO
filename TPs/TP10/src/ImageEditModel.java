e1b918fb0ea9630fadf960f3dfae0942159e48da
Yag000 <80483460+Yag000@users.noreply.github.com>
2022-12-06 16:57:42 +0100

    Tp10 (#17)
    * Initial commit
    
    * Exo 2
    
    * Exo 3
    
    * Exo 4.1
    
    * Exo 4
    
    * Exo 5
    
    * Exo 6
    
    * Exo 7
    
    * Working exo 7
    
    * Exo 8.1
    
    * Exo 8.2
    
    * Exo 9.1
    
    * Exo 9.2
    
    * Exo 9.3
    
    * Exo 10.1
    
    * Exo 10.2
    
    * Exo 11.1
    
    * Exo 11.2
    
    * Exo 11.3
    
    * Couper + Undo work
    
    * Organize imports
    
    * Working version
    
    * Rename clearzone -> clearZone
    
    * Add sujet pdf
    
    * Add javadoc
    
    * Better selection rectangle
    
    * final commit

---
+++ TPs/TP10/src/ImageEditModel.java
@@ -0,0 +1,154 @@
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
+
+/**
+ * Model de l'application. Cette classe permet de gérer le coupage del'image et
+ * les undo et redo.
+ */
+public class ImageEditModel {
+
+    BufferedImage image;
+
+    UndoManager undoManager = new UndoManager();
+
+    // Constructeur
+
+    public ImageEditModel(String chemin) {
+        try {
+            this.image = ImageIO.read(new File(chemin));
+        } catch (Exception e) {
+            System.out.println("Erreur lors de la lecture de l'image");
+        }
+    }
+
+    // Classes internes
+
+    /**
+     * Cette classe represente un coupe d'image. Elle permet de undo et redo le
+     * coupage.
+     */
+    private class Coupe {
+        Rectangle z; // Zone de la coupe
+        int[][] pixels; // Pixels de l'image
+
+        Coupe(int x, int y, int width, int height, BufferedImage image) {
+            this.z = new Rectangle(x, y, width, height);
+            this.pixels = new int[width][height];
+
+            // Boucle pour obtenir les pixels de l'image à couper
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    this.pixels[i][j] = image.getRGB(i, j);
+        }
+
+        /**
+         * Cette méthode permet de couper l'image.
+         */
+        void doit() {
+            ImageEditModel.this.clearZone(z);
+        }
+
+        /**
+         * Cette méthode permet d'annuler une coupe.
+         */
+        void undo() {
+            ImageEditModel.this.fillzone(z, pixels);
+        }
+    }
+
+    /**
+     * Classe auxiliare pour une coupe, elle étend {@code AbstractUndoableEdit} ce
+     * qui permet d'utiliser un {@code UndoManager} pour gérer les undo et redo de
+     * coupe.
+     */
+    private class CutEdit extends AbstractUndoableEdit {
+        Coupe c;
+
+        public CutEdit(Coupe c) {
+            this.c = c;
+        }
+
+        @Override
+        public void redo() throws CannotRedoException {
+            c.doit();
+        }
+
+        @Override
+        public void undo() throws CannotUndoException {
+            c.undo();
+        }
+    }
+
+    // Getter
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    // Méthodes
+
+    /**
+     * Permet de remplier une zone {@code z} avec une nouvelle image encodé sous
+     * forme d'un array de pixels
+     * 
+     * @param z      Zone a remplir
+     * @param pixels Array qui contient l'image
+     */
+    public void fillzone(Rectangle z, int[][] pixels) {
+
+        if (z.getWidth() != pixels.length)
+            throw new IllegalArgumentException("La zone et le tableau ne sont pas de la même taille");
+
+        for (int[] ligne : pixels)
+            if (z.getHeight() != ligne.length)
+                throw new IllegalArgumentException("La zone et le tableau ne sont pas de la même taille");
+
+        for (int i = 0; i < z.getWidth(); i++)
+            for (int j = 0; j < z.getHeight(); j++)
+                image.setRGB((int) z.getX() + i, (int) z.getY() + j, pixels[i][j]);
+    }
+
+    /**
+     * Rempli une zone {@code z} avec la couleur blanche.
+     * 
+     * @param z Zone à remplir
+     */
+    public void clearZone(Rectangle z) {
+        Color color = Color.white;
+        int srgb = color.getRGB();
+
+        for (int i = 0; i < z.getWidth(); i++)
+            for (int j = 0; j < z.getHeight(); j++)
+                image.setRGB((int) z.getX() + i, (int) z.getY() + j, srgb);
+    }
+
+    /**
+     * Cette méthode permet de sauvegarder une coupe, pour pouvoir couper et, après,
+     * annuler ou refaire cette action.
+     * 
+     * @param z Zone a couper
+     */
+    public void saveCut(Rectangle z) {
+        BufferedImage subImage = image.getSubimage((int) z.getX(), (int) z.getY(), (int) z.getWidth(),
+                (int) z.getHeight());
+
+        Coupe c = new Coupe((int) z.getX(), (int) z.getY(), (int) z.getWidth(), (int) z.getHeight(), subImage);
+
+        c.doit();
+
+        CutEdit ce = new CutEdit(c);
+
+        undoManager.addEdit(ce);
+    }
+
+}
---
+++ TPs/TP10/src/ImageEditView.java
@@ -0,0 +1,180 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.undo.CannotRedoException;
+
+/**
+ * Implementation graphique de l’application.
+ */
+public class ImageEditView extends JFrame {
+
+    JButton cutButton; // Button qui controle l'action de couper
+    JButton undoButton;// Button qui controle l'undo
+    JButton redoButton;// Button qui controle le redo
+
+    ImagePane imagePane;// Le pane principal de l'application
+    ImageEditModel model;// Model de l'application
+
+    // Constructeur
+
+    public ImageEditView(ImageEditModel model) {
+        this.model = model;
+
+        setTitle("Mon éditeur d'image");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JMenuBar menuBar = new JMenuBar();
+
+        setJMenuBar(menuBar);
+
+        initButtons(menuBar);
+
+        imagePane = new ImagePane();
+
+        setContentPane(imagePane);
+    }
+
+    // Classes membres
+
+    /**
+     * Classe qui controle les actions de l'utilisateur sur l'image.
+     */
+    private class ImagePane extends JPanel {
+
+        Selection selection = new Selection();
+
+        public ImagePane() {
+            setPreferredSize(new Dimension(model.getImage().getWidth(), model.getImage().getHeight()));
+
+            addMouseListener(selection);
+            addMouseMotionListener(selection);
+        }
+
+        /**
+         * Classe qui controle les actions de l'utilisateur sur la selection.
+         */
+        private class Selection extends MouseAdapter implements MouseMotionListener {
+
+            int x0;
+            int x1;
+            int y0;
+            int y1;
+
+            public Rectangle getRectangle() {
+                return new Rectangle(Math.min(x0, x1), Math.min(y0, y1), Math.abs(x1 - x0), Math.abs(y1 - y0));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent event) {
+                x0 = event.getX();
+                y0 = event.getY();
+
+                // On fait attention a ne pas sortir de l'image
+                x0 = Math.min(x0, model.getImage().getWidth());
+                y0 = Math.min(y0, model.getImage().getHeight());
+
+                x0 = Math.max(x0, 0);
+                y0 = Math.max(y0, 0);
+
+                x1 = x0;
+                y1 = y0;
+
+                cutButton.setEnabled(false);
+
+                imagePane.repaint();
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent event) {
+                x1 = event.getX();
+                y1 = event.getY();
+
+                // On fait attention a ne pas sortir de l'image
+                x1 = Math.min(x1, model.getImage().getWidth());
+                y1 = Math.min(y1, model.getImage().getHeight());
+
+                x1 = Math.max(x1, 0);
+                y1 = Math.max(y1, 0);
+
+                if (x0 != x1 || y0 != y1)
+                    cutButton.setEnabled(true);
+
+                imagePane.repaint();
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent event) {
+            }
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(model.getImage(), 0, 0, this);
+            ((Graphics2D) g).draw(selection.getRectangle());
+        }
+    }
+
+    // Helper methods
+
+    /**
+     * Méthode pour initialiser les buttons
+     * 
+     * @param menuBar Le menu où les buttons seront ajoutés
+     */
+    private void initButtons(JMenuBar menuBar) {
+
+        cutButton = new JButton("Cut");
+        undoButton = new JButton("Undo");
+        redoButton = new JButton("Redo");
+        JButton quitButton = new JButton("Quit");
+
+        cutButton.setEnabled(false);
+        undoButton.setEnabled(false);
+        redoButton.setEnabled(false);
+
+        cutButton.addActionListener(e -> {
+            model.saveCut(imagePane.selection.getRectangle());
+
+            imagePane.repaint();
+
+            cutButton.setEnabled(false);
+            undoButton.setEnabled(true);
+            redoButton.setEnabled(true);
+        });
+
+        undoButton.addActionListener(e -> {
+            if (model.undoManager.canUndo()) {
+                model.undoManager.undo();
+            }
+            imagePane.repaint();
+        });
+
+        redoButton.addActionListener(e -> {
+            try {
+                model.undoManager.redo();
+            } catch (CannotRedoException ex) {
+                // On essaye de redo l'action et on catch l'exception
+                // qui se lève si l'action est possible
+            }
+            imagePane.repaint();
+        });
+
+        quitButton.addActionListener(e -> System.exit(0));
+
+        menuBar.add(cutButton);
+        menuBar.add(undoButton);
+        menuBar.add(redoButton);
+        menuBar.add(quitButton);
+    }
+}
