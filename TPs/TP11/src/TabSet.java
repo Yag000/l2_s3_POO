083db63517b184f5e66bcf85bf73be855b718285
Yag000 <80483460+Yag000@users.noreply.github.com>
2022-12-12 14:06:41 +0100

    Tp11 (#19)
    * Add javadoc
    
    * Add sujet
    
    * Add test
    
    * RemoveAll now works and add comments
    
    * Format text
    
    * TestFunction is now an interface
    
    * Format code and add comments
    
    * Add now prints the false case

--- TPs/TP11/src/TabSet.java
+++ TPs/TP11/src/TabSet.java
@@ -1,40 +1,37 @@
 import java.lang.reflect.Array;
+
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
 
-class TabSet<E> implements Iterable<E>, Set<E> {
+class TabSet<E> implements Set<E> {
+
+    // L'interface Set<E> étend l'interface Collection<E> qui étend l'interface
+    // Iterable<E> c'est pour cela qu'il suffit d'implémenter l'interface
+    // Iterable<E> pour que notre classe TabSet<E> soit un ensemble d'éléments de
+    // type E qui est itérable.
 
     private E[] tableau;
 
+    // Constructeur
     /**
-     * Constructeur prenant en paramètre la capacité initiale du TabSet
-     * 
-     * @param n la capacité initiale du TabSet
+     * Constructeur. Il construit un nouveau ensemble vide. Le tableau interne est
+     * de taille 10.
      */
     @SuppressWarnings("unchecked")
     public TabSet() {
         tableau = (E[]) new Object[10];
     }
 
-    /**
-     * Renvoie un itérateur permettant de parcourir les éléments du TabSet
-     * 
-     * @return un itérateur pour parcourir les éléments du TabSet
-     */
+    // Méthodes de l'interface Iterable<E>
+
     @Override
     public TabIter iterator() {
         return new TabIter();
     }
 
-    /**
-     * Renvoie true si l'élément passé en paramètre est présent dans le TabSet,
-     * false
-     * sinon
-     * 
-     * @param o l'élément dont on veut savoir s'il est présent dans le TabSet
-     * @return true si l'élément est présent dans le TabSet, false sinon
-     */
+    // Méthodes de l'interface Set<E>
+
     @Override
     public boolean contains(Object o) {
         for (E e : this) {
@@ -44,11 +41,6 @@ class TabSet<E> implements Iterable<E>, Set<E> {
         return false;
     }
 
-    /**
-     * Renvoie la taille du TabSet, c'est-à-dire le nombre d'éléments qu'il contient
-     * 
-     * @return la taille du TabSet
-     */
     @Override
     public int size() {
         int count = 0;
@@ -65,7 +57,18 @@ class TabSet<E> implements Iterable<E>, Set<E> {
         return size() == 0;
     }
 
-    private int findIndex(E e) {
+    /**
+     * Cherche la premiere case vide du tableau.
+     * 
+     * Pour cette fonction, et seulement pour cette fonction, on n'utilise pas
+     * l'iterator car il saute les éléments nuls. Or comme on a besoin de trouver le
+     * premier élément nul, on ne peut pas utiliser l'iterator. C'est pour cela
+     * qu'on utilise {@code tableau}.
+     *
+     * @return l'index de la premiere case vide du tableau, ou {@code -1} si le
+     *         tableau est plein.
+     */
+    private int findIndex() {
         int counter = 0;
 
         for (E element : tableau) {
@@ -78,23 +81,39 @@ class TabSet<E> implements Iterable<E>, Set<E> {
         return -1;
     }
 
+    @Override
     public boolean add(E e) {
-        if (contains(e))
+        // Si l'élément est null, on ne l'ajoute pas
+        if (e == null) {
+            System.out.println("L'élément est null");
             return false;
+        }
 
-        int pos = findIndex(e);
+        // Si l'élément est déjà présent dans le tableau, on ne l'ajoute pas
+        if (contains(e)) {
+            System.out.println("L'élément est déjà present");
+            return false;
+        }
 
+        // On cherche la première case vide du tableau
+        int pos = findIndex();
+
+        // Si le tableau est plein, on le redimensionne
         if (pos == -1) {
             int length = tableau.length;
             resize();
             pos = length;
         }
 
+        // On ajoute l'élément à la première case vide du tableau
         tableau[pos] = e;
         return true;
     }
 
+    @Override
     public boolean remove(Object o) {
+        // On parcourt le TabSet avec un itérateur et si on trouve l'élément à
+        // supprimer, on le supprime avec la méthode remove et on renvoie true
 
         Iterator<E> it = iterator();
         while (it.hasNext()) {
@@ -110,6 +129,8 @@ class TabSet<E> implements Iterable<E>, Set<E> {
 
     @Override
     public void clear() {
+        // On parcourt le TabSet avec un itérateur et on supprime tous les
+        // éléments avec la méthode remove
         Iterator<E> it = iterator();
         while (it.hasNext()) {
             it.next();
@@ -154,14 +175,27 @@ class TabSet<E> implements Iterable<E>, Set<E> {
     public boolean removeAll(Collection<?> c) {
         boolean wasModified = false;
         for (Object e : c) {
-            if (remove(c)) {
+            if (remove(e)) {
                 wasModified = true;
             }
         }
         return wasModified;
     }
 
+    /**
+     * Affiche tous les éléments de la collection sur la sortie standard.
+     */
     public void affiche() {
+        for (E e : this) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Affiche tous les éléments de la collection et les éléments nuls sur la sortie
+     * standard.
+     */
+    public void afficheAll() {
         for (E e : tableau) {
             System.out.println(e);
         }
@@ -177,15 +211,6 @@ class TabSet<E> implements Iterable<E>, Set<E> {
         return tab;
     }
 
-    /**
-     * Remplit le tableau passé en paramètre avec les éléments du TabSet, et met
-     * null ensuite s'il y a suffisamment de place dans le tableau. Si le tableau
-     * n'est pas assez grand, un nouveau tableau de même type que a est créé et
-     * renvoyé.
-     * 
-     * @param a le tableau à remplir avec les éléments du {@code TabSet}
-     * @return le tableau rempli avec les éléments du {@code TabSet}
-     */
     @Override
     @SuppressWarnings("unchecked")
     public <T> T[] toArray(T[] a) {
@@ -194,7 +219,6 @@ class TabSet<E> implements Iterable<E>, Set<E> {
 
         // Création d'un nouveau tableau de type T[] qui contiendra les éléments du
         // TabSet
-
         T[] newArray = (T[]) Array.newInstance(c, size());
 
         // Copie des éléments du TabSet dans le nouveau tableau
@@ -217,6 +241,10 @@ class TabSet<E> implements Iterable<E>, Set<E> {
             return newArray;
     }
 
+    /**
+     * Redimensionne le tableau interne de la collection pour qu'il puisse contenir
+     * deux fois plus d'éléments.
+     */
     @SuppressWarnings("unchecked")
     private void resize() {
         E[] newArray = (E[]) new Object[tableau.length * 2];
@@ -227,22 +255,33 @@ class TabSet<E> implements Iterable<E>, Set<E> {
         tableau = newArray;
     }
 
+    // Classe interne privée
+
+    /**
+     * Classe interne privée permettant d'itérer sur les éléments du {@code TabSet}.
+     */
     private class TabIter implements Iterator<E> {
 
-        private int index;
+        private int currentIndex;
 
+        // Constructeur
+        /**
+         * Initialise l'index de l'itérateur à 0.
+         */
         public TabIter() {
-            index = 0;
+            currentIndex = 0;
         }
+
+        // Méthodes
 
         @Override
         public boolean hasNext() {
-            if (index >= tableau.length)
+            if (currentIndex >= tableau.length)
                 return false;
 
             // On parcourt le tableau à partir de la position courante jusqu'à la fin du
             // tableau
-            for (int i = index; i < tableau.length; i++) {
+            for (int i = currentIndex; i < tableau.length; i++) {
                 // Si on rencontre un élément null, cela signifie qu'il n'y a plus d'éléments à
                 // parcourir
                 if (tableau[i] != null)
@@ -253,10 +292,17 @@ class TabSet<E> implements Iterable<E>, Set<E> {
             return false;
         }
 
+        /**
+         * Cherche l'index du prochain élément non null dans le tableau.
+         *
+         * @return l'index du prochain élément non null dans le tableau, ou la taille du
+         *         tableau si aucun
+         *         élément non null n'est trouvé.
+         */
         private int findNextIndex() {
             // On parcourt le tableau à partir de la position courante jusqu'à la fin du
             // tableau
-            for (int i = index + 1; i < tableau.length; i++) {
+            for (int i = currentIndex + 1; i < tableau.length; i++) {
                 // Si on rencontre un élément non null, on renvoie son index
                 if (tableau[i] != null)
                     return i;
@@ -269,8 +315,8 @@ class TabSet<E> implements Iterable<E>, Set<E> {
         @Override
         public E next() {
             if (hasNext()) {
-                int lastIndex = index;
-                index = findNextIndex();
+                int lastIndex = currentIndex;
+                currentIndex = findNextIndex();
 
                 return tableau[lastIndex];
             } else {
@@ -278,9 +324,15 @@ class TabSet<E> implements Iterable<E>, Set<E> {
             }
         }
 
+        /**
+         * Cherche l'index du dernier élément non null dans le tableau.
+         *
+         * @return l'index du dernier élément non null dans le tableau, ou {@code -1} si
+         *         aucun élément non null n'est trouvé.
+         */
         private int findLastIndex() {
 
-            for (int i = index - 1; i >= 0; i--) {
+            for (int i = currentIndex - 1; i >= 0; i--) {
                 if (tableau[i] != null)
                     return i;
             }
@@ -291,7 +343,7 @@ class TabSet<E> implements Iterable<E>, Set<E> {
         @Override
         public void remove() {
 
-            if (index == 0)
+            if (currentIndex == 0)
                 throw new IllegalStateException();
 
             int lastIndex = findLastIndex();
--- TPs/TP11/src/Test.java
+++ TPs/TP11/src/Test.java
@@ -1,136 +1,187 @@
 import java.util.Arrays;
 import java.util.Iterator;
 
+import utilities.TestFunction;
+
 public class Test {
     public static void main(String[] args) {
-        Integer[] tableau = new Integer[] { 1, 2, 3, 4, 5 };
-        TestIter<Integer> iter = new TestIter<>(tableau);
+        new TestFunction() {
+            @Override
+            public void function() {
+                Integer[] tableau = new Integer[] { 1, 2, 3, 4, 5 };
+                TestIter<Integer> iter = new TestIter<>(tableau);
 
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
-        }
+                while (iter.hasNext()) {
+                    System.out.println(iter.next());
+                }
+            }
+        }.runTest("TestIter");
 
-        System.out.println("--------------------- Test TabSet ---------------------");
+        new TestFunction() {
+            @Override
+            public void function() {
 
-        TabSet<String> set1 = new TabSet<>();
+                TabSet<String> set1 = new TabSet<>();
 
-        // Ajout de quelques éléments au TabSet
-        set1.add("A");
-        set1.add("B");
-        set1.add("C");
+                // Ajout de quelques éléments au TabSet
+                set1.add("A");
+                set1.add("B");
+                set1.add("C");
 
-        set1.affiche();
+                set1.affiche();
 
-        Iterator<String> iter2 = set1.iterator();
-        System.out.println("Test de l'itérateur: " + iter2.hasNext());
+                Iterator<String> iter2 = set1.iterator();
+                System.out.println("Test de l'itérateur: " + iter2.hasNext()); // Affiche : Test de l'itérateur: true
 
-        // Affichage de la taille actuelle du TabSet
-        System.out.println("Taille actuelle du TabSet : " + set1.size()); // Affiche : Taille actuelle du TabSet : 3
+                System.out.println("Taille actuelle du TabSet : " + set1.size()); // Affiche : Taille actuelle du TabSet
+                                                                                  // : 3
 
-        // Vérification si le TabSet contient l'élément "A"
-        System.out.println("Contient A : " + set1.contains("A")); // Affiche : Contient A : true
+                // Vérification si le TabSet contient l'élément "A"
+                System.out.println("Contient A : " + set1.contains("A")); // Affiche : Contient A : true
+            }
+        }.runTest("Add, size et contains");
 
-        // Suppression de l'élément "B" du TabSet
-        set1.remove("B");
+        new TestFunction() {
+            @Override
+            public void function() {
+                TabSet<String> set1 = new TabSet<>();
 
-        // Vérification si le TabSet contient toujours l'élément "B"
-        System.out.println("Contient B : " + set1.contains("B")); // Affiche : Contient B : false
+                // Ajout de quelques éléments au TabSet
+                set1.add("A");
+                set1.add("B");
+                set1.add("C");
 
-        // Itération sur les éléments du TabSet
-        for (String element : set1) {
-            System.out.println(element);
-        }
-        // Affiche :
-        // A
-        // C
+                System.out.println("Avant: ");
 
-        System.out.println("--------------------- Test TabSet 2 ---------------------");
+                set1.affiche();
 
-        TabSet<String> set2 = new TabSet<>();
+                // Suppression de l'élément "B" du TabSet
+                set1.remove("B");
 
-        // Ajout de quelques éléments au TabSet
-        set2.add("A");
-        set2.add("B");
-        set2.add("C");
-        set2.add(null);
-        set2.add("D");
-        set2.add(null);
+                // Vérification si le TabSet contient toujours l'élément "B"
+                System.out.println("Contient B : " + set1.contains("B")); // Affiche : Contient B : false
 
-        // Itération sur les éléments du TabSet
-        for (String element : set2) {
-            System.out.println(element);
-        }
-        // Affiche :
-        // A
-        // B
-        // C
-        // D
+                System.out.println("Après: ");
+                set1.affiche(); // Affiche : A C
+            }
+        }.runTest("Remove");
 
-        System.out.println("Size: " + set2.size()); // Affiche : Size: 4
+        new TestFunction() {
+            @Override
+            public void function() {
+                TabSet<String> set2 = new TabSet<>();
 
-        System.out.println("--------------------- Test TabSet 3 ---------------------");
+                // Ajout de quelques éléments au TabSet
+                set2.add("A");
+                set2.add("B");
+                set2.add("C");
+                set2.add(null);
+                set2.add("D");
+                set2.add(null);
 
-        TabSet<String> set3 = new TabSet<>();
+                set2.afficheAll(); // Affiche : A B C D null null null null null null null
 
-        // Ajout de quelques éléments au TabSet
-        set3.add("A");
-        set3.add("B");
-        set3.add("C");
+                System.out.println("Size: " + set2.size()); // Affiche : Size: 4
+            }
+        }.runTest("Size avec des null");
 
-        // Création d'un autre TabSet contenant les éléments "B" et "D"
-        TabSet<String> set4 = new TabSet<>();
-        set4.add("B");
-        set4.add("D");
+        new TestFunction() {
+            @Override
+            public void function() {
+                TabSet<String> set3 = new TabSet<>();
 
-        // Vérification si le TabSet contient tous les éléments de l'autre TabSet
-        System.out.println(set3.containsAll(set4)); // Doit renvoyer false
+                set3.add("A");
+                set3.add("B");
+                set3.add("C");
 
-        // Ajout de tous les éléments de l'autre TabSet au TabSet principal
-        System.out.println(set3.addAll(set4)); // Doit renvoyer true
+                // Création d'un autre TabSet contenant les éléments "B" et "D"
+                TabSet<String> set4 = new TabSet<>();
+                set4.add("B");
+                set4.add("D");
 
-        // Vérification si le TabSet contient maintenant tous les éléments de l'autre
-        // TabSet
-        System.out.println(set3.containsAll(set4)); // Doit renvoyer true
+                // Vérification si le TabSet contient tous les éléments de l'autre TabSet
+                System.out.println(set3.containsAll(set4)); // Doit renvoyer false
 
-        // Création d'un troisième TabSet contenant les éléments "A" et "C"
-        TabSet<String> thirdSet = new TabSet<>();
-        thirdSet.add("A");
-        thirdSet.add("C");
+                // Ajout de tous les éléments de l'autre TabSet au TabSet principal
+                System.out.println(set3.addAll(set4)); // Doit renvoyer true
+                System.out.println();
 
-        // Garder seulement les éléments communs entre le TabSet principal et le
-        // troisième TabSet
-        System.out.println(set3.retainAll(thirdSet)); // Doit renvoyer true
+                set3.affiche(); // Doit afficher A B C D
 
-        // Vérification de la taille du TabSet après l'opération de "retain"
-        System.out.println(set3.size() == 2); // Doit renvoyer true
+                // Vérification si le TabSet contient maintenant tous les éléments de l'autre
+                // TabSet
+                System.out.println(set3.containsAll(set4)); // Doit renvoyer true
 
-        // Suppression de tous les éléments du TabSet
-        set3.clear();
+                // Création d'un troisième TabSet contenant les éléments "A" et "C"
+                TabSet<String> thirdSet = new TabSet<>();
+                thirdSet.add("A");
+                thirdSet.add("C");
 
-        // Vérification que le TabSet est vide après l'appel à la méthode clear()
-        System.out.println(set3.isEmpty()); // Doit renvoyer true
+                // Garder seulement les éléments communs entre le TabSet principal et le
+                // troisième TabSet
+                System.out.println(set3.retainAll(thirdSet)); // Doit renvoyer true
+                System.out.println();
+                set3.affiche(); // Doit afficher A C
 
-        System.out.println("--------------------- Test TabSet 4 ---------------------");
+                // On ajoute a nouveau les éléments de set4
+                set3.addAll(set4);
+                System.out.println();
+                set3.affiche(); // Doit afficher A B C D
 
-        TabSet<String> tabSet = new TabSet<>();
+                // On supprime les éléments de set4
+                set3.removeAll(set4);
+                System.out.println();
+                set3.affiche(); // Doit afficher A C
 
-        // Ajout d'éléments dans le TabSet
-        tabSet.add("Hello");
-        tabSet.add("World");
-        tabSet.add("!");
+                // Vérification de la taille du TabSet après l'opération retain
+                System.out.println(set3.size() == 2); // Doit renvoyer true
 
-        // Test de la méthode toArray()
-        Object[] array = tabSet.toArray();
-        System.out.println(Arrays.toString(array)); // [Hello, World, !]
+                // Suppression de tous les éléments du TabSet
+                set3.clear();
 
-        // Test de la méthode toArray(T[] a) avec un tableau assez grand
-        String[] array2 = new String[5];
-        String[] array3 = tabSet.toArray(array2);
-        System.out.println(Arrays.toString(array3)); // [Hello, World, !, null, null]
+                // Vérification que le TabSet est vide après l'appel à la méthode clear()
+                System.out.println(set3.isEmpty()); // Doit renvoyer true
+            }
+        }.runTest("containsAll, addAll, retainsAll, removeAll");
 
-        // Test de la méthode toArray(T[] a) avec un tableau trop petit
-        String[] array4 = new String[2];
-        String[] array5 = tabSet.toArray(array4);
-        System.out.println(Arrays.toString(array5)); // [Hello, World, !]
+        new TestFunction() {
+            @Override
+            public void function() {
+                TabSet<String> tabSet = new TabSet<>();
+
+                tabSet.add("Hello");
+                tabSet.add("World");
+                tabSet.add("!");
+
+                // Test de la méthode toArray()
+                Object[] array = tabSet.toArray();
+                System.out.println(Arrays.toString(array)); // [Hello, World, !]
+
+                // Test de la méthode toArray(T[] a) avec un tableau assez grand
+                String[] array2 = new String[5];
+                String[] array3 = tabSet.toArray(array2);
+                System.out.println(Arrays.toString(array3)); // [Hello, World, !, null, null]
+
+                // Test de la méthode toArray(T[] a) avec un tableau trop petit
+                String[] array4 = new String[2];
+                String[] array5 = tabSet.toArray(array4);
+                System.out.println(Arrays.toString(array5)); // [Hello, World, !]
+            }
+        }.runTest("toArray");
+
+        new TestFunction() {
+            @Override
+            public void function() {
+                TabSet<Integer> ts = new TabSet<>();
+
+                // On ajoute plus d'éléments que la capacité initiale du tableau
+                for (int i = 0; i < 15; i++) {
+                    ts.add(i);
+                }
+
+                // On affiche le contenu du TabSet
+                ts.affiche();
+            }
+        }.runTest("Tableau de taille dynamique");
     }
 }
