c31442f436ea980b32daddca67da1375305e607e
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-12 17:07:37 +0100

    Exo 4

--- TPs/TP12/src/partie1/Cadre.java
+++ TPs/TP12/src/partie1/Cadre.java
@@ -2,7 +2,10 @@ package partie1;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
 public class Cadre extends JFrame {
 
@@ -13,21 +16,62 @@ public class Cadre extends JFrame {
         this.setTitle("TP 12");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        mainPanel = new JPanel();
-        mainPanel.setLayout(null);
-
+        mainPanel = new JPanel(null);
         setContentPane(mainPanel);
 
         Carre c = new Carre();
         mainPanel.add(c);
     }
 
-    private class Carre extends JPanel {
+    private class Carre extends JPanel implements MouseInputListener {
 
         public Carre() {
 
             setBackground(Color.BLUE);
             setBounds(100, 200, 50, 50);
+            addMouseListener(this);
+
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+            System.out.println("Click");
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            // TODO Auto-generated method stub
 
         }
 
