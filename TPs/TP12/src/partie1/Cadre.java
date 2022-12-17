ef6a2c495499c28e2580115b9fcdae788257ba8f
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-17 22:44:12 +0100

    Add javadoc

--- TPs/TP12/src/partie1/Cadre.java
+++ TPs/TP12/src/partie1/Cadre.java
@@ -8,6 +8,11 @@ import javax.swing.event.MouseInputListener;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+/**
+ * Constructeur de la classe Cadre. Initialise la taille, le titre et la
+ * fermeture de la fenêtre,
+ * ainsi que les carrés à afficher.
+ */
 public class Cadre extends JFrame {
     JPanel etiquette;
     JPanel mainPanel;
@@ -15,6 +20,10 @@ public class Cadre extends JFrame {
     Model model = new Model();
     Carre[] carres;
 
+    /**
+     * Constructeur de la classe Cadre. Initialise la taille, le titre et la
+     * fermeture de la fenêtre, ainsi que les carrés à afficher.
+     */
     public Cadre() {
         this.setSize(600, 600);
         this.setTitle("TP 12");
@@ -31,10 +40,21 @@ public class Cadre extends JFrame {
         }
     }
 
+    /**
+     * Méthode qui vérifie si l'utilisateur a gagné la partie.
+     * 
+     * @return true si l'utilisateur a gagné, false sinon.
+     */
     public boolean gagne() {
         return model.gagne();
     }
 
+    /**
+     * Méthode qui affiche un message de félicitation lorsque l'utilisateur a gagné
+     * la partie.
+     * Ajoute également un écouteur d'événement de souris pour quitter l'application
+     * lorsque l'utilisateur clique sur le message.
+     */
     public void finJeu() {
         if (!gagne())
             return;
@@ -56,12 +76,12 @@ public class Cadre extends JFrame {
 
             @Override
             public void mouseDragged(MouseEvent e) {
-
+                //
             }
 
             @Override
             public void mouseMoved(MouseEvent e) {
-
+                //
             }
 
             @Override
@@ -71,28 +91,37 @@ public class Cadre extends JFrame {
 
             @Override
             public void mousePressed(MouseEvent e) {
-
+                //
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-
+                //
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-
+                //
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
+                //
             }
 
         });
 
     }
 
+    /**
+     * Classe interne représentant un carré affiché dans la fenêtre de
+     * l'application.
+     * Cette classe implémente l'interface MouseInputListener pour gérer les
+     * événements de souris.
+     */
     private class Carre extends JPanel implements MouseInputListener {
+
+        private static boolean isMouseOnASquare = false;
 
         static int nb = 0;
 
@@ -102,6 +131,11 @@ public class Cadre extends JFrame {
         int xClick;
         int yClick;
 
+        /**
+         * Constructeur de la classe Carre. Initialise l'identifiant du carré, sa
+         * couleur, sa position aléatoire et
+         * les écouteurs d'événements de souris.
+         */
         public Carre() {
             id = nb++;
 
@@ -112,13 +146,23 @@ public class Cadre extends JFrame {
 
         }
 
+        /**
+         * Méthode appelée lorsque l'utilisateur clique sur un carré.
+         * Change la couleur du carré en vert et vérifie si l'utilisateur a gagné la
+         * partie.
+         */
+
         @Override
         public void mouseClicked(MouseEvent e) {
+            if (isMouseOnASquare != isMoving)
+                return;
 
             xClick = e.getX();
             yClick = e.getY();
+
             isMoving = !isMoving;
-            System.out.println(isMoving);
+            isMouseOnASquare = isMoving;
+
             model.setColor(id, Color.GREEN);
             setBackground(Color.GREEN);
             finJeu();
@@ -127,17 +171,22 @@ public class Cadre extends JFrame {
 
         @Override
         public void mousePressed(MouseEvent e) {
+            //
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            // TODO Auto-generated method stub
-
+            //
         }
 
+        /**
+         * Méthode appelée lorsque la souris entre dans un carré.
+         * Change la couleur du carré en bleu et vérifie si l'utilisateur a gagné la
+         * partie.
+         */
         @Override
         public void mouseEntered(MouseEvent e) {
-            if (!isMoving) {
+            if (!isMouseOnASquare) {
                 model.setColor(id, Color.BLUE);
                 setBackground(Color.BLUE);
                 finJeu();
@@ -147,19 +196,20 @@ public class Cadre extends JFrame {
 
         @Override
         public void mouseExited(MouseEvent e) {
-            // TODO Auto-generated method stub
-
+            //
         }
 
         @Override
         public void mouseDragged(MouseEvent e) {
-            // TODO Auto-generated method stub
-
+            //
         }
 
+        /**
+         * Méthode appelée lorsque la souris se déplace sur un carré.
+         * Elle actualise la position du carré s'il est en mouvement..
+         */
         @Override
         public void mouseMoved(MouseEvent e) {
-
             if (isMoving) {
                 int x = getX() + e.getX() - xClick;
                 int y = getY() + e.getY() - yClick;
@@ -168,6 +218,110 @@ public class Cadre extends JFrame {
             }
         }
 
+    }
+
+    /**
+     * Classe représentant le modèle du jeu, gérant les carrés et vérifiant si
+     * l'utilisateur a gagné.
+     */
+
+    private class Model {
+
+        Color[] colors;
+
+        /**
+         * Constructeur de la classe Model. Initialise le nombre de couleurs
+         * utilisées dans l'application et crée le tableau de couleurs.
+         */
+        public Model() {
+
+            int random = (int) (Math.random() * 10 + 1);
+
+            colors = new Color[random];
+
+            for (int i = 0; i < random; i++) {
+                colors[i] = intToColor(i + 1);
+            }
+
+        }
+
+        /**
+         * Méthode qui convertit un entier en couleur.
+         * 
+         * @param i l'entier à convertir en couleur.
+         * @return la couleur correspondant à l'entier.
+         */
+        private Color intToColor(int i) {
+            switch (i) {
+                case 1:
+                    return Color.CYAN;
+                case 2:
+                    return Color.DARK_GRAY;
+                case 3:
+                    return Color.RED;
+                case 4:
+                    return Color.YELLOW;
+                case 5:
+                    return Color.ORANGE;
+                case 6:
+                    return Color.MAGENTA;
+                case 7:
+                    return Color.LIGHT_GRAY;
+                case 8:
+                    return Color.PINK;
+                case 9:
+                    return Color.GRAY;
+                case 10:
+                    return Color.GREEN;
+                default:
+                    return Color.BLUE;
+            }
+        }
+
+        /**
+         * Méthode qui retourne la couleur d'un carré du tableau {@code colors}.
+         * 
+         * @param i l'indice du carré à récupérer dans le tableau {@code colors}.
+         * @return la couleur du carré.
+         */
+        public Color getColor(int i) {
+            return colors[i];
+        }
+
+        /**
+         * Méthode qui retourne le nombre de couleurs différentes utilisées dans le jeu.
+         * 
+         * @return le nombre de couleurs différentes utilisées dans le jeu.
+         */
+        public int getNbColors() {
+            return colors.length;
+        }
+
+        /**
+         * Méthode qui permet de changer la couleur d'un carré du tableau
+         * {@code colors}.
+         * 
+         * @param i     l'indice du carré à modifier dans le tableau {@code colors}.
+         * @param color la nouvelle couleur du carré.
+         */
+        public void setColor(int i, Color c) {
+            colors[i] = c;
+        }
+
+        /**
+         * Méthode qui vérifie si l'utilisateur a gagné la partie.
+         * 
+         * @return true si l'utilisateur a gagné, false sinon.
+         */
+        public boolean gagne() {
+            for (int i = 0; i < colors.length; i++) {
+                if (colors[i] != colors[0]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 
     public static void main(String[] args) {
