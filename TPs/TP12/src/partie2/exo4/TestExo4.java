acae329ba14f0165e80a00ed30604cb57346740a
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-17 19:13:33 +0100

    Exo 5 bis

--- TPs/TP12/src/partie2/exo4/TestExo4.java
+++ TPs/TP12/src/partie2/exo4/TestExo4.java
@@ -1,9 +1,9 @@
-package partie2.exo2;
+package partie2.exo4;
 
 import partie2.exo2.MonPack.A;
 import partie2.exo2.MonPack.MonSousPack.B;
 
-public class Test {
+public class TestExo4 {
     public static void main(String[] args) {
         A a = new A();
         B b = new B();
