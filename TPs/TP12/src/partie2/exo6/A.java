8454186682f13c341b78acbd99d96b88b331481d
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-17 19:20:33 +0100

    Exo 6 bis

---
+++ TPs/TP12/src/partie2/exo6/A.java
@@ -0,0 +1,12 @@
+package partie2.exo6;
+
+public class A {
+
+    protected void printP() {
+        System.out.println("A");
+    }
+
+    void print() {
+        System.out.println("A");
+    }
+}
